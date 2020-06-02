8006d44a30edb30f2d8c1aea650919f677953e3e
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-06-03 00:24:50 +0200

    Navigation Stuff

---
+++ app/src/main/java/com/jaqxues/sniptools/ui/views/fragments/NavFragment.kt
@@ -0,0 +1,14 @@
+package com.jaqxues.sniptools.ui.views.fragments
+
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
+ * Date: 02.06.20 - Time 23:44.
+ */
+abstract class NavFragment : Fragment() {
+    @get:IdRes
+    abstract val menuId: Int
+}
---
+++ app/src/main/java/com/jaqxues/sniptools/ui/views/ui/views/DynamicNavigationView.kt
@@ -0,0 +1,89 @@
+package com.jaqxues.sniptools.ui.views.ui.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.SparseArray
+import android.view.MenuItem
+import androidx.annotation.IdRes
+import androidx.core.view.isNotEmpty
+import com.google.android.material.navigation.NavigationView
+import com.jaqxues.sniptools.ui.views.fragments.NavFragment
+import timber.log.Timber
+
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
+ * Date: 02.06.20 - Time 23:05.
+ */
+
+class DynamicNavigationView : NavigationView, NavigationView.OnNavigationItemSelectedListener {
+    private var currentMenuItem: MenuItem? = null
+    private val fragments = SparseArray<NavFragment>()
+    private lateinit var activeFragment: NavFragment
+    private lateinit var listener: NavigationFragmentListener
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+    /**
+     * @param action: Adds Fragments and returns the currently active Fragment
+     */
+    fun initialize(listener: NavigationFragmentListener, action: DynamicNavigationView.() -> NavFragment?) {
+        this.listener = listener
+        setNavigationItemSelectedListener(this)
+
+        if (menu.isNotEmpty())
+            checkMenuItem(menu.getItem(0))
+
+        action()?.let {
+            activeFragment = it
+            listener.selectedFragment(it)
+        }
+    }
+
+    private fun checkMenuItem(item: MenuItem) {
+        item.isChecked = true
+        currentMenuItem?.isChecked = false
+        currentMenuItem = item
+    }
+
+    fun addFragment(fragment: NavFragment) {
+        fragments.append(fragment.menuId, fragment)
+    }
+
+    fun removeFragment(@IdRes menuId: Int) = fragments.remove(menuId)
+
+    fun getFragmentById(@IdRes menuId: Int) =
+        fragments.get(menuId) ?: throw IllegalArgumentException("MenuId not associated with a fragment")
+
+    fun navigate(@IdRes menuId: Int): Boolean {
+        val item = menu.findItem(menuId)
+        if (item == null) {
+            Timber.w("Tried to navigate to Id ($menuId) but item was not found")
+            return false
+        }
+        return onNavigationItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val selected = fragments.get(item.itemId) ?: return false
+        if (selected == activeFragment) return true
+
+        checkMenuItem(item)
+        listener.selectedFragment(selected)
+        activeFragment = selected
+
+        return true
+    }
+
+    operator fun NavFragment.unaryPlus() {
+        addFragment(this)
+    }
+
+    interface NavigationFragmentListener {
+        fun selectedFragment(fragment: NavFragment)
+    }
+}
