0185ae4379c4595677acc03b864feadefe5c290f
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-06-03 18:12:24 +0200

    Compress PackIcon, add Strings and PackMetadata.kt

---
+++ app/src/main/java/com/jaqxues/sniptools/data/PackMetadata.kt
@@ -0,0 +1,47 @@
+package com.jaqxues.sniptools.data
+
+import com.jaqxues.akrolyb.pack.IPackMetadata
+
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project SnapTools.<br>
+ * Date: 27.05.20 - Time 08:54.
+ * Moved to SnipTools on Date: 03.06.20 - Time 17:45.
+ */
+interface PackMetadata : IPackMetadata {
+    val isTutorial: Boolean
+    val latest: Boolean
+    val flavour: String
+    val scVersion: String
+    val name: String
+}
+
+data class ServerPackMetadata(
+    override val devPack: Boolean,
+    override val packVersion: String,
+    override val packVersionCode: Int,
+    override val packImplClass: String,
+    override val minApkVersionCode: Int,
+    override val isTutorial: Boolean,
+    override val latest: Boolean,
+    override val flavour: String,
+    override val scVersion: String,
+    override val name: String,
+
+    val description: String,
+    val isInstalled: Boolean,
+    val hasUpdate: Boolean
+) : PackMetadata
+
+data class LocalPackMetadata(
+    override val isTutorial: Boolean,
+    override val latest: Boolean,
+    override val flavour: String,
+    override val scVersion: String,
+    override val name: String,
+    override val devPack: Boolean,
+    override val packVersion: String,
+    override val packVersionCode: Int,
+    override val packImplClass: String,
+    override val minApkVersionCode: Int
+) : PackMetadata
