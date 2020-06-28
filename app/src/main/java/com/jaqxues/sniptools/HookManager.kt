f54f01544dc4cb57a474ccbf3364076d1e61e427
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-06-28 20:37:08 +0200

    No Signature Security Check for Debug Builds

--- app/src/main/java/com/jaqxues/sniptools/HookManager.kt
+++ app/src/main/java/com/jaqxues/sniptools/HookManager.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.os.Bundle
 import com.jaqxues.akrolyb.pack.ModPackBase
 import com.jaqxues.akrolyb.prefs.getPref
+import com.jaqxues.akrolyb.utils.Security
 import com.jaqxues.sniptools.data.Preferences.SELECTED_PACKS
 import com.jaqxues.sniptools.pack.ModPack
 import com.jaqxues.sniptools.pack.PackFactory
@@ -61,7 +62,7 @@ class HookManager : IXposedHookLoadPackage {
                     val pack: ModPack = ModPackBase.buildPack(
                         moduleContext,
                         File(PathProvider.modulesPath, selectedPack),
-//                        Security.certificateFromApk(moduleContext, CustomApplication.PACKAGE_NAME),
+                        if (BuildConfig.DEBUG) null else Security.certificateFromApk(moduleContext, CustomApplication.PACKAGE_NAME),
                         packBuilder = PackFactory(true)
                     )
                     val featureManager = pack.featureManager
