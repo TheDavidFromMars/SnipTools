5c34d6a69ab39d748912290f284af5e01ec52594
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-07-03 18:15:38 +0200

    Remove Debug Database

--- packimpl/src/main/java/com/jaqxues/sniptools/packimpl/ForcedHooks.kt
+++ packimpl/src/main/java/com/jaqxues/sniptools/packimpl/ForcedHooks.kt
@@ -1,13 +1,8 @@
 package com.jaqxues.sniptools.packimpl
 
 import android.content.Context
-import com.amitshekhar.DebugDB
-import com.amitshekhar.debug.sqlite.DebugDBFactory
 import com.jaqxues.sniptools.fragments.BaseFragment
 import com.jaqxues.sniptools.pack.IFeature
-import com.jaqxues.sniptools.utils.ContextContainer
-import com.jaqxues.sniptools.utils.after
-import de.robv.android.xposed.XposedHelpers
 
 
 /**
@@ -23,16 +18,5 @@ class ForcedHooks : IFeature() {
         get() = TODO("Not yet implemented")
 
     override fun loadFeature(classLoader: ClassLoader, context: Context) {
-
-
-        XposedHelpers.findAndHookMethod("com.snap.media.provider.MediaPackageFileProvider", classLoader, "onCreate", after {
-            if (BuildConfig.DEBUG) {
-                DebugDB.initialize(
-                    ContextContainer.getModuleContextNotNull(context),
-                    context,
-                    DebugDBFactory()
-                )
-            }
-        })
     }
 }
