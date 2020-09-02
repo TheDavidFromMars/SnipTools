f72c21877825b5236db9b1755e1411889fdd6d94
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-09-02 13:51:50 +0200

    PackManagerFragment.kt UI Changes

--- app/src/main/java/com/jaqxues/sniptools/fragments/PackManagerFragment.kt
+++ app/src/main/java/com/jaqxues/sniptools/fragments/PackManagerFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animate
+import androidx.compose.animation.*
 import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
@@ -123,6 +120,7 @@ fun PackElement(packName: String) {
             .fillMaxWidth(),
         elevation = 4.dp
     ) {
+        var isActive by remember { mutableStateOf(false) }
         val packData = remember {
             PackData(
                 packName,
@@ -141,6 +139,7 @@ fun PackElement(packName: String) {
                 Icon(
                     vectorResource(id = R.drawable.ic_pack),
                     Modifier.padding(horizontal = 4.dp)
+                        .gravity(Alignment.CenterVertically)
                         .preferredHeight(8.dp)
                         .drawWithContent {
                             rotate(rotation) {
@@ -151,35 +150,45 @@ fun PackElement(packName: String) {
                 Icon(
                     vectorResource(id = R.drawable.ic_pack),
                     Modifier.padding(horizontal = 16.dp)
-                        .preferredHeight(24.dp)
+                        .preferredHeight(24.dp),
+                    tint = animate(if (isActive) Color(0xFF00AA00) else Color.White)
                 )
                 Text(
                     text = "Pack v$packName"
                 )
             }
             AnimatedVisibility(visible = extendedPack) {
-                ExtendedPackLayout(packData)
+                ExtendedPackLayout(packData, isActive) { isActive = it }
             }
         }
     }
 }
 
 @Composable
-fun ExtendedPackLayout(packData: PackData) {
+fun ExtendedPackLayout(packData: PackData, isActive: Boolean, onCheckedChange: (Boolean) -> Unit) {
     Column(Modifier.padding(horizontal = 16.dp).fillMaxWidth()) {
         Divider(Modifier.padding(horizontal = 20.dp))
-        Row(Modifier.gravity(Alignment.CenterHorizontally)) {
-            repeat(3) {
-                IconButton(onClick = {}) {
-                    Image(
-                        vectorResource(id = R.drawable.ic_pack),
-                        colorFilter = ColorFilter.tint(Color.White),
-                        modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
-                    )
-                }
+        Row(Modifier.gravity(Alignment.CenterHorizontally), verticalGravity = Alignment.CenterVertically) {
+            IconButton(onClick = {}) {
+                Image(
+                    vectorResource(id = R.drawable.ic_baseline_history_48),
+                    colorFilter = ColorFilter.tint(Color.White),
+                    modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
+                )
+            }
+
+            Switch(isActive, onCheckedChange = onCheckedChange, color = Color(0xFF00AA00), modifier = Modifier.padding(horizontal = 16.dp))
+
+            IconButton(onClick = {}) {
+                Image(
+                    vectorResource(id = R.drawable.ic_baseline_remove_circle_outline_48),
+                    colorFilter = ColorFilter.tint(Color.White),
+                    modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
+                )
             }
         }
         Divider(Modifier.padding(horizontal = 80.dp))
+
         Spacer(Modifier.padding(8.dp))
         Text("Pack Type: ${packData.packType}", fontSize = 12.sp, color = Color.LightGray)
         Text("Snapchat Version: ${packData.scVersion}", fontSize = 12.sp, color = Color.LightGray)
