f114657254c83340fd1fb223f4b92723a76a4ada
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-09-06 20:38:29 +0200

    Pack Repository and ViewModel

--- app/src/main/java/com/jaqxues/sniptools/repository/PackRepository.kt
+++ app/src/main/java/com/jaqxues/sniptools/repository/PackRepository.kt
@@ -1,16 +1,20 @@
 package com.jaqxues.sniptools.repository
 
+import android.content.Context
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.jaqxues.sniptools.data.PackMetadata
-import com.jaqxues.sniptools.data.ServerPackMetadata
+import com.jaqxues.akrolyb.prefs.add
+import com.jaqxues.akrolyb.prefs.remove
+import com.jaqxues.sniptools.data.Preferences.SELECTED_PACKS
+import com.jaqxues.sniptools.data.StatefulPackData
 import com.jaqxues.sniptools.networking.GitHubApiService
-import com.jaqxues.sniptools.utils.PackUtils
+import com.jaqxues.sniptools.pack.PackFactory
+import com.jaqxues.sniptools.pack.PackLoadManager
 import com.jaqxues.sniptools.utils.PathProvider
-import com.jaqxues.sniptools.utils.Request
 import timber.log.Timber
 import java.io.File
+import java.security.cert.X509Certificate
 
 
 /**
@@ -19,33 +23,80 @@ import java.io.File
  * Moved to SnipTools on Date: 03.06.20 - Time 17:42.
  */
 class PackRepository(private val retrofit: GitHubApiService) {
-    // Private Mutable LiveData
-    private val _localMetadata = MutableLiveData<List<PackMetadata>>()
-    private val _remoteMetadata = MutableLiveData<Request<List<ServerPackMetadata>>>()
-
-    // Exposed public LiveData
-    val localMetadata: LiveData<List<PackMetadata>> = _localMetadata
-    val remoteMetadata: LiveData<Request<List<ServerPackMetadata>>> = _remoteMetadata
+    init {
+        PackLoadManager.registerListener(this::onPackStateChanged)
+    }
 
     private val packDirectory = File(PathProvider.modulesPath)
+    private val loadablePackStates = mutableMapOf<String, MutableLiveData<StatefulPackData>>()
+
+    // Private Mutable LiveData
+    private val _localPacks = MutableLiveData<List<String>>(emptyList())
+
+    // Exposed public LiveData
+    val localPacks: LiveData<List<String>> = _localPacks
 
     @WorkerThread
-    fun refreshLocalMetadata() {
-        val jarFileList = packDirectory.listFiles { pathName -> pathName.isFile && pathName.extension == "jar" }
+    suspend fun refreshLocalPacks(context: Context, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        val jarFileList =
+            packDirectory.listFiles { file -> file.isFile && file.extension == "jar" }
 
         if (jarFileList.isNullOrEmpty()) {
-            _localMetadata.postValue(emptyList())
+            _localPacks.postValue(emptyList())
             return
         }
+        refreshPackStates(jarFileList, context, certificate, packBuilder)
+        _localPacks.postValue(jarFileList.map(File::getName))
+    }
 
-        val packs = jarFileList.associate { file ->
-            val metadata = try {
-                PackUtils.getPackMetadata(file)
+    suspend fun refreshPackStates(files: Array<File>, context: Context, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        for (packFile in files) {
+            try {
+                PackLoadManager.loadState(context, packFile, certificate, packBuilder)
             } catch (t: Throwable) {
                 Timber.e(t, "Could not load Metadata from Pack Attributes")
-                throw t
             }
-            metadata.name to metadata
         }
+    }
+
+    suspend fun activatePack(
+        context: Context,
+        packFile: File,
+        certificate: X509Certificate? = null,
+        packBuilder: PackFactory
+    ) {
+        SELECTED_PACKS.add(packFile.name)
+        try {
+            PackLoadManager.requestLoadPack(context, packFile, certificate, packBuilder)
+        } catch (t: Throwable) {
+            Timber.e(t,"Could not activate Pack")
+        }
+    }
+
+    suspend fun deactivatePack(context: Context, packFile: File, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        SELECTED_PACKS.remove(packFile.name)
+        PackLoadManager.requestUnloadPack(context, packFile, certificate, packBuilder)
+    }
+
+    fun getStateFor(packFileName: String): LiveData<StatefulPackData> {
+        return loadablePackStates.getOrPut(packFileName) {
+            MutableLiveData(PackLoadManager.getStateFor(packFileName))
+        }
+    }
+
+    fun deletePack(packFile: File) {
+        packFile.delete()
+        PackLoadManager.deletePackState(packFile.name)
+        loadablePackStates.remove(packFile.name)
+        _localPacks.postValue(_localPacks.value!!.let {
+            val list = it.toMutableList()
+            list.remove(packFile.name)
+            list
+        })
+    }
+
+    private fun onPackStateChanged(packFileName: String, state: StatefulPackData) {
+        if (packFileName in loadablePackStates)
+            loadablePackStates.getValue(packFileName).postValue(state)
     }
 }
--- app/src/main/java/com/jaqxues/sniptools/viewmodel/PackViewModel.kt
+++ app/src/main/java/com/jaqxues/sniptools/viewmodel/PackViewModel.kt
@@ -1,9 +1,16 @@
 package com.jaqxues.sniptools.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jaqxues.sniptools.data.StatefulPackData
+import com.jaqxues.sniptools.pack.PackFactory
 import com.jaqxues.sniptools.repository.PackRepository
+import kotlinx.coroutines.launch
+import java.io.File
+import java.security.cert.X509Certificate
 import kotlin.random.Random
 
 
@@ -11,9 +18,33 @@ import kotlin.random.Random
  * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
  * Date: 05.06.20 - Time 19:41.
  */
-class PackViewModel(private val packRepo: PackRepository): ViewModel() {
-    val lastChecked: LiveData<Long> = MutableLiveData(System.currentTimeMillis() - Random.nextLong(0, 1e8.toLong()))
+class PackViewModel(private val packRepo: PackRepository) : ViewModel() {
 
-    val localPacks: LiveData<List<String>> = MutableLiveData(listOf("10.41.6.0", "10.89.7.72", "10.23.62.24"))
-    val serverPacks: LiveData<List<String>> = MutableLiveData(listOf("11.0.0.0", "11.23.0.3", "12.32.3.23"))
+    val lastChecked: LiveData<Long> =
+        MutableLiveData(System.currentTimeMillis() - Random.nextLong(0, 1e8.toLong()))
+
+    val localPacks: LiveData<List<String>> = packRepo.localPacks
+
+    fun getStateDataForPack(packFileName: String): LiveData<StatefulPackData> =
+        packRepo.getStateFor(packFileName)
+
+    fun refreshLocalPacks(context: Context, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        viewModelScope.launch {
+            packRepo.refreshLocalPacks(context, certificate, packBuilder)
+        }
+    }
+
+    fun activatePack(context: Context, packFile: File, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        viewModelScope.launch {
+            packRepo.activatePack(context, packFile, certificate, packBuilder)
+        }
+    }
+
+    fun deactivatePack(context: Context, packFile: File, certificate: X509Certificate? = null, packBuilder: PackFactory) {
+        viewModelScope.launch {
+            packRepo.deactivatePack(context, packFile, certificate, packBuilder)
+        }
+    }
+
+    fun deletePack(packFile: File) = packRepo.deletePack(packFile)
 }
