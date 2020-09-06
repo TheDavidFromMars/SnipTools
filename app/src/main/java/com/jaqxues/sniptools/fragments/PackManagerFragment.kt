ff2880dbc76f007a69c28596e2dd780673feb8ad
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-09-06 20:39:01 +0200

    PackSelector.kt UI

--- app/src/main/java/com/jaqxues/sniptools/fragments/PackManagerFragment.kt
+++ app/src/main/java/com/jaqxues/sniptools/fragments/PackManagerFragment.kt
@@ -1,7 +1,6 @@
 package com.jaqxues.sniptools.fragments
 
 import android.os.Bundle
-import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,32 +9,23 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animate
 import androidx.compose.foundation.Icon
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.contentColor
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.TabConstants.defaultTabIndicatorOffset
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.drawWithContent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ContextAmbient
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.annotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
 import com.jaqxues.sniptools.R
 import com.jaqxues.sniptools.ui.AppScreen
@@ -57,7 +47,6 @@ class PackManagerFragment : BaseFragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 PackManagerScreen()
-
             }
         }
     }
@@ -91,90 +80,30 @@ fun PackManagerScreen() {
             Crossfade(current = currentTab) {
                 when (it) {
                     PackManagerTabs.PACK_SELECTOR -> PackSelectorTab(packViewModel)
-                    PackManagerTabs.PACK_DOWNLOADER -> PackDownloaderTab(packViewModel)
+                    PackManagerTabs.PACK_DOWNLOADER -> Unit //PackDownloaderTab(packViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun PackSelectorTab(packViewModel: PackViewModel) {
-    val localPacks by packViewModel.localPacks.observeAsState()
-    LazyColumnFor(items = localPacks!!) {
-        PackElement(it)
-    }
-}
-
-@Composable
-fun PackDownloaderTab(packViewModel: PackViewModel) {
-    val serverPacks by packViewModel.serverPacks.observeAsState()
-    val lastChecked by packViewModel.lastChecked.observeAsState()
-
-    Column(Modifier.fillMaxHeight(), horizontalGravity = Alignment.CenterHorizontally) {
-        LazyColumnFor(items = serverPacks!!, modifier = Modifier.weight(1f)) {
-            PackElement(it)
-        }
-
-        Divider(Modifier.padding(48.dp, 8.dp, 48.dp), color = colorResource(id = R.color.colorPrimary))
-        Text(annotatedString {
-            append("Last Checked: ")
-            withStyle(SpanStyle(colorResource(R.color.colorPrimaryLight))) {
-                append(DateUtils.getRelativeDateTimeString(
-                    ContextAmbient.current, lastChecked!!,
-                    DateUtils.SECOND_IN_MILLIS, DateUtils.WEEK_IN_MILLIS,
-                    DateUtils.FORMAT_ABBREV_RELATIVE
-                ).toString())
-            }
-        }, modifier = Modifier.padding(8.dp), color = Color.LightGray, fontSize = 12.sp)
-    }
-}
-
-
-@Preview
-@Composable
-fun PreviewPack() {
-    AppScreen {
-        PackElement(packName = "11.0.0.72")
-    }
-}
-
-data class PackData(
-    val packName: String,
-    val packType: String,
-    val scVersion: String,
-    val packVersion: String
-)
-
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun PackElement(packName: String) {
-    var extendedPack by remember { mutableStateOf(false) }
-    Card(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .padding(4.dp)
-            .clip(RoundedCornerShape(8.dp))
-            .clickable(onClick = { extendedPack = !extendedPack })
-            .fillMaxWidth(),
-        elevation = 4.dp
-    ) {
-        var isActive by remember { mutableStateOf(false) }
-        val packData = remember {
-            PackData(
-                packName,
-                arrayOf("Premium", "Basic").random(),
-                packName,
-                packName
-            )
-        }
+fun ExpandablePackLayout(
+    packName: String,
+    color: Color = contentColor(),
+    extendedContent: @Composable () -> Unit
+) {
+    var extended by remember { mutableStateOf(false) }
+    ListCardElement(onClick = { extended = !extended }) {
         Column {
             Row(
                 Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
             ) {
-                val rotation = animate(if (extendedPack) 0f else 180f)
+                val rotation = animate(if (extended) 0f else 180f)
+
                 Icon(
                     vectorResource(id = R.drawable.ic_pack),
                     Modifier.padding(horizontal = 4.dp)
@@ -186,53 +115,35 @@ fun PackElement(packName: String) {
                             }
                         }
                 )
+
                 Icon(
                     vectorResource(id = R.drawable.ic_pack),
                     Modifier.padding(horizontal = 16.dp)
                         .preferredHeight(24.dp),
-                    tint = animate(if (isActive) Color(0xFF00AA00) else Color.White)
+                    tint = color
                 )
                 Text(
-                    text = "Pack v$packName"
+                    text = packName
                 )
             }
-            AnimatedVisibility(visible = extendedPack) {
-                ExtendedPackLayout(packData, isActive) { isActive = it }
+
+            AnimatedVisibility(visible = extended) {
+                extendedContent()
             }
         }
     }
 }
 
 @Composable
-fun ExtendedPackLayout(packData: PackData, isActive: Boolean, onCheckedChange: (Boolean) -> Unit) {
-    Column(Modifier.padding(horizontal = 16.dp).fillMaxWidth()) {
-        Divider(Modifier.padding(horizontal = 20.dp))
-        Row(Modifier.gravity(Alignment.CenterHorizontally), verticalGravity = Alignment.CenterVertically) {
-            IconButton(onClick = {}) {
-                Image(
-                    vectorResource(id = R.drawable.ic_baseline_history_48),
-                    colorFilter = ColorFilter.tint(Color.White),
-                    modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
-                )
-            }
-
-            Switch(isActive, onCheckedChange = onCheckedChange, color = Color(0xFF00AA00), modifier = Modifier.padding(horizontal = 16.dp))
-
-            IconButton(onClick = {}) {
-                Image(
-                    vectorResource(id = R.drawable.ic_baseline_remove_circle_outline_48),
-                    colorFilter = ColorFilter.tint(Color.White),
-                    modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
-                )
-            }
-        }
-        Divider(Modifier.padding(horizontal = 80.dp))
-
-        Spacer(Modifier.padding(8.dp))
-        Text("Pack Type: ${packData.packType}", fontSize = 12.sp, color = Color.LightGray)
-        Text("Snapchat Version: ${packData.scVersion}", fontSize = 12.sp, color = Color.LightGray)
-        Text("Pack Version: ${packData.packVersion}", fontSize = 12.sp, color = Color.LightGray)
-        Spacer(Modifier.padding(8.dp))
+fun ListCardElement(onClick: (() -> Unit)? = null, content: @Composable () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(4.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .run { onClick?.let { clickable(onClick = it) } ?: this }
+            .fillMaxWidth(), elevation = 4.dp) {
+        content()
     }
 }
 
---
+++ app/src/main/java/com/jaqxues/sniptools/fragments/PackSelector.kt
@@ -0,0 +1,166 @@
+package com.jaqxues.sniptools.fragments
+
+import androidx.compose.animation.animate
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollableColumn
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.ColumnScope.gravity
+import androidx.compose.material.Divider
+import androidx.compose.material.IconButton
+import androidx.compose.material.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jaqxues.sniptools.R
+import com.jaqxues.sniptools.data.PackMetadata
+import com.jaqxues.sniptools.data.StatefulPackData
+import com.jaqxues.sniptools.pack.PackFactory
+import com.jaqxues.sniptools.viewmodel.PackViewModel
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
+ * Date: 06.09.20 - Time 19:30.
+ */
+
+@Composable
+fun PackSelectorTab(packViewModel: PackViewModel) {
+    packViewModel.refreshLocalPacks(ContextAmbient.current, null, PackFactory(false))
+    val localPacks by packViewModel.localPacks.observeAsState()
+
+    ScrollableColumn {
+        localPacks!!.forEach {
+            val packData by packViewModel.getStateDataForPack(it).run {
+                observeAsState(value!!)
+            }
+            PackElementLayout(packData, packViewModel)
+        }
+    }
+}
+
+@Composable
+fun PackElementLayout(packData: StatefulPackData, packViewModel: PackViewModel) {
+    when (packData) {
+        is StatefulPackData.CorruptedPack -> ExpandablePackLayout(
+            packName = packData.packFile.name,
+            color = Color.Red
+        ) {
+            Text(
+                packData.message,
+                Modifier.padding(horizontal = 24.dp, vertical = 8.dp),
+                fontSize = 12.sp
+            )
+        }
+        else -> {
+            val color = animate(
+                when (packData) {
+                    is StatefulPackData.AvailablePack ->
+                        Color.White
+                    is StatefulPackData.LoadedPack ->
+                        Color(0xFF00AA00)
+                    is StatefulPackData.PackLoadError ->
+                        Color.Red
+                    else -> error("Illegal PackLoad State")
+                }
+            )
+
+            ExpandablePackLayout(packName = packData.packMetadata.name, color = color) {
+                Column(Modifier.padding(horizontal = 16.dp).fillMaxWidth()) {
+                    Divider(Modifier.padding(horizontal = 20.dp))
+
+
+                    val context = ContextAmbient.current
+                    IconButtonRow(packData, onChangelog = {
+
+                    }, onChangeActive = {
+                        if (it) {
+                            packViewModel.activatePack(
+                                context,
+                                packData.packFile,
+                                null,
+                                PackFactory(false)
+                            )
+                        } else {
+                            if (packData.isActive) {
+                                packViewModel.deactivatePack(
+                                    context,
+                                    packData.packFile,
+                                    null,
+                                    PackFactory(false)
+                                )
+                            }
+                        }
+                    }, onDelete = {
+                        packViewModel.deletePack(packData.packFile)
+                    })
+                    Divider(Modifier.padding(horizontal = 80.dp))
+
+                    Spacer(Modifier.padding(8.dp))
+                    PackMetadataLayout(metadata = packData.packMetadata)
+
+                    if (packData is StatefulPackData.PackLoadError) {
+                        Text(packData.message, Modifier.padding(vertical = 8.dp), fontSize = 12.sp)
+                    } else {
+                        Spacer(Modifier.padding(8.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun IconButtonRow(
+    packData: StatefulPackData,
+    modifier: Modifier = Modifier,
+    onChangelog: () -> Unit,
+    onChangeActive: (Boolean) -> Unit,
+    onDelete: () -> Unit
+) {
+    Row(
+        modifier.gravity(Alignment.CenterHorizontally),
+        verticalGravity = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = onChangelog) {
+            Image(
+                vectorResource(id = R.drawable.ic_baseline_history_48),
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
+            )
+        }
+
+        Switch(
+            packData.isActive,
+            onCheckedChange = onChangeActive,
+            color = if (packData !is StatefulPackData.PackLoadError) Color(0xFF00AA00) else Color.Red,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        IconButton(onClick = onDelete) {
+            Image(
+                vectorResource(id = R.drawable.ic_baseline_remove_circle_outline_48),
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier = Modifier.preferredHeight(40.dp).padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun PackMetadataLayout(metadata: PackMetadata) {
+    Text(
+        "Pack Type: ${if (metadata.devPack) "Developer" else "User"}",
+        fontSize = 12.sp,
+        color = Color.LightGray
+    )
+    Text("Snapchat Version: ${metadata.scVersion}", fontSize = 12.sp, color = Color.LightGray)
+    Text("Pack Version: ${metadata.packVersion}", fontSize = 12.sp, color = Color.LightGray)
+}
