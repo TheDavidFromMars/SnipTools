d5d57f0138bca449c5443c6135ac598640ffb1f9
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-09-10 22:53:15 +0200

    Use Neutral Button for Spacing in Screenshot Dialog

--- packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/ScreenshotBypass.kt
+++ packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/ScreenshotBypass.kt
@@ -55,7 +55,7 @@ class ScreenshotBypass : IFeature() {
                             dialog.dismiss()
                         }
 
-                        setNegativeButton("No") { dialog, _ ->
+                        setNeutralButton("No") { dialog, _ ->
                             dialog.dismiss()
                         }
 
