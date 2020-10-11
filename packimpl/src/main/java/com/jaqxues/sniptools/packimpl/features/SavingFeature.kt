d750eb78d382587dfbd59cd9401c343d92eba7b3
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-10-11 12:15:46 +0200

    Option to disable AutoSaving

--- packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/SavingFeature.kt
+++ packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/SavingFeature.kt
@@ -2,9 +2,11 @@ package com.jaqxues.sniptools.packimpl.features
 
 import android.content.Context
 import com.jaqxues.akrolyb.genhook.decs.after
-import com.jaqxues.sniptools.fragments.PackFragment
+import com.jaqxues.akrolyb.prefs.getPref
 import com.jaqxues.sniptools.pack.IFeature
+import com.jaqxues.sniptools.packimpl.fragment.SavingFragment
 import com.jaqxues.sniptools.packimpl.hookdec.MemberDeclarations.DECRYPT_MEDIA_STREAM
+import com.jaqxues.sniptools.packimpl.utils.PackPreferences.AUTO_SAVE_SNAPS
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.InputStream
@@ -15,23 +17,24 @@ import java.io.InputStream
  * Date: 24.08.20 - Time 13:12.
  */
 class SavingFeature : IFeature() {
-    override fun getFragments() = emptyArray<PackFragment>()
+    override fun getFragments() = arrayOf(SavingFragment())
 
     override fun loadFeature(classLoader: ClassLoader, context: Context) {
-        hookMethod(DECRYPT_MEDIA_STREAM, after {
-            val result = it.result as InputStream
-            val bytes = result.use(InputStream::readBytes)
+        if (AUTO_SAVE_SNAPS.getPref())
+            hookMethod(DECRYPT_MEDIA_STREAM, after {
+                val result = it.result as InputStream
+                val bytes = result.use(InputStream::readBytes)
 
-            val folder = File("/storage/emulated/0/SnipTools/Test").apply {
-                if (!exists()) mkdirs()
-            }
+                val folder = File("/storage/emulated/0/SnipTools/Test").apply {
+                    if (!exists()) mkdirs()
+                }
 
-            File(folder, "Auto_Save_${System.currentTimeMillis()}_${System.nanoTime()}.jpg").apply {
-                createNewFile()
-                writeBytes(bytes)
-            }
+                File(folder, "Auto_Save_${System.currentTimeMillis()}_${System.nanoTime()}.jpg").apply {
+                    createNewFile()
+                    writeBytes(bytes)
+                }
 
-            it.result = ByteArrayInputStream(bytes)
-        })
+                it.result = ByteArrayInputStream(bytes)
+            })
     }
 }
---
+++ packimpl/src/main/java/com/jaqxues/sniptools/packimpl/fragment/SavingFragment.kt
@@ -0,0 +1,39 @@
+package com.jaqxues.sniptools.packimpl.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import com.jaqxues.sniptools.fragments.PackFragment
+import com.jaqxues.sniptools.packimpl.utils.PackPreferences.AUTO_SAVE_SNAPS
+import com.jaqxues.sniptools.packimpl.utils.SwitchPreference
+import com.jaqxues.sniptools.ui.AppScreen
+
+/**
+ * This file was created by Jacques Hoffmann (jaqxues) in the Project SnipTools.<br>
+ * Date: 10.10.20 - Time 12:13.
+ */
+class SavingFragment: PackFragment() {
+    override val name = "Saving"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setContent {
+            AppScreen {
+                Column(Modifier.padding(16.dp)) {
+                    SwitchPreference(AUTO_SAVE_SNAPS) {
+                        Text("Auto Save Snaps")
+                    }
+                }
+            }
+        }
+    }
+}
