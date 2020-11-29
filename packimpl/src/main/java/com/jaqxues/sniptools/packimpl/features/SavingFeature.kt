597b780756d7369ae77d6bef966167511a13f734
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-11-29 13:13:58 +0100

    Update Saving (saving mp4/jpg)

--- packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/SavingFeature.kt
+++ packimpl/src/main/java/com/jaqxues/sniptools/packimpl/features/SavingFeature.kt
@@ -1,12 +1,16 @@
 package com.jaqxues.sniptools.packimpl.features
 
 import android.content.Context
+import com.jaqxues.akrolyb.genhook.decs.AddInsField
 import com.jaqxues.akrolyb.genhook.decs.after
 import com.jaqxues.akrolyb.prefs.getPref
 import com.jaqxues.sniptools.pack.IFeature
 import com.jaqxues.sniptools.packimpl.fragment.Destinations
-import com.jaqxues.sniptools.packimpl.hookdec.MemberDeclarations.DECRYPT_MEDIA_STREAM
+import com.jaqxues.sniptools.packimpl.hookdec.MemberDeclarations
 import com.jaqxues.sniptools.packimpl.utils.PackPreferences.AUTO_SAVE_SNAPS
+import de.robv.android.xposed.XposedHelpers.findAndHookMethod
+import de.robv.android.xposed.XposedHelpers.getObjectField
+import timber.log.Timber
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.InputStream
@@ -20,21 +24,38 @@ class SavingFeature : IFeature() {
     override fun getDestinations() = arrayOf(Destinations.SAVING.destination)
 
     override fun loadFeature(classLoader: ClassLoader, context: Context) {
-        if (AUTO_SAVE_SNAPS.getPref())
-            hookMethod(DECRYPT_MEDIA_STREAM, after {
+        if (AUTO_SAVE_SNAPS.getPref()) {
+            val isVideoAddIns = AddInsField<Boolean>()
+            findAndHookMethod("vJ5", classLoader, "t", "g66 ", after {
+                it.result?.let { result ->
+                    val isVideo = when (val type = getObjectField(it.args[0], "b").toString()) {
+                        "IMAGE" -> false
+                        "VIDEO", "VIDEO_NO_SOUND" -> true
+                        else -> {
+                            Timber.d("Unsupported Media Type %s", type)
+                            return@let
+                        }
+                    }
+                    isVideoAddIns.set(result, isVideo)
+                }
+            })
+
+            hookMethod(MemberDeclarations.DECRYPT_MEDIA_STREAM, after {
+                val ext = if (!(isVideoAddIns.get(it.thisObject) ?: return@after)) "jpg" else "mp4"
                 val result = it.result as InputStream
                 val bytes = result.use(InputStream::readBytes)
 
                 val folder = File("/storage/emulated/0/SnipTools/Test").apply {
                     if (!exists()) mkdirs()
                 }
-
-                File(folder, "Auto_Save_${System.currentTimeMillis()}_${System.nanoTime()}.jpg").apply {
-                    createNewFile()
-                    writeBytes(bytes)
-                }
+                File(folder, "Auto_Save_${System.currentTimeMillis()}_${System.nanoTime()}.$ext")
+                    .apply {
+                        createNewFile()
+                        writeBytes(bytes)
+                    }
 
                 it.result = ByteArrayInputStream(bytes)
             })
+        }
     }
 }
